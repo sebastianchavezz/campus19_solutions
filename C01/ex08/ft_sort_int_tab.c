06e98bdde83ec75daaee3edd68a3467fdfe98068
sebastianchavezz <sebastianchavez940@gmail.com>
2023-02-28 21:04:03 +0100

    laatse

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,8 +6,30 @@
 /*   By: schavez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 20:02:27 by schavez           #+#    #+#             */
-/*   Updated: 2023/02/28 20:02:34 by schavez          ###   ########.fr       */
+/*   Updated: 2023/02/28 21:01:21 by schavez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
 
+	i = 0;
+	while (i < size -1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (*(tab + i) > *(tab + j))
+			{
+				temp = *(tab + j);
+				*(tab + j) = *(tab + i);
+				*(tab + i) = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
